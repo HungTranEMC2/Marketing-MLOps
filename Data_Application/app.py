e6f5b958a3433a6cfdad32bf01dfeb0d7988d71d
Hung Tran <52383372+HungTranEMC2@users.noreply.github.com>
2024-10-04 18:13:29 +0000

    fix bug

--- Data_Application/app.py
+++ Data_Application/app.py
@@ -36,9 +36,9 @@ if selected == "Home":
    st.title('Welcome to My Marketing MLOps Project Home Page')
    st.header('Black Friday Sales Analysis and Prediction App')
    current_path = os.getcwd()
-   print('current path: ', current_path)
+   st.markdown(f'current_path')
    #st.image('/workspaces/Marketing-MLOps/Data_Application/MLOps.png',caption = 'MLOps Project')
-   st.image(os.path.join(os.getcwd(), 'MLOps.png'), caption= 'MLOps Project')
+   #st.image(os.path.join(os.getcwd(), 'MLOps.png'), caption= 'MLOps Project')
    st.markdown("""
    This project aims to analyze and predict Black Friday sales using advanced machine learning operations (MLOps). 
    The Exploration and Analysis will show the visualization of the dataset.
