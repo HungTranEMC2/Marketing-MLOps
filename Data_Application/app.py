03cc6b6d96fd4be0f796eda6a545d6662bb8bc20
HungTranEMC2 <52383372+HungTranEMC2@users.noreply.github.com>
2025-01-22 20:36:13 -0600

    feat: delete pkl file
    delete pkl file

--- Data_Application/app.py
+++ Data_Application/app.py
@@ -11,7 +11,7 @@ import os
 
 def get_data():
     # Import the dataset
-    data = pd.read_csv('/mount/src/marketing-mlops/Data_Engineer/data/marketing.csv')
+    data = pd.read_csv('/Users/alextran/Documents/GitHub/Marketing-MLOps/Data_Engineer/data/marketing.csv')
     data.drop(columns = ['Unnamed: 0'], inplace= True)
     return data
 
@@ -36,7 +36,7 @@ with st.sidebar:
 if selected == "Home":
    st.title('Welcome to My Marketing Data Science Project Home Page')
    st.header('Black Friday Sales Analysis and Prediction App')
-   st.image('/mount/src/marketing-mlops/Data_Application/MLOps.png', caption ='MLOps Project')
+   st.image('/Users/alextran/Documents/GitHub/Marketing-MLOps/Data_Application/MLOps.png', caption ='MLOps Project')
    st.markdown("""
    This project aims to analyze and predict Black Friday sales using advanced machine learning operations (MLOps). 
    The Exploration and Analysis will show the visualization of the dataset.
